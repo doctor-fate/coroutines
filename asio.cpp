3e5642935d41dac96156fa53bcbf71acd0823ba5
achumakov <achumakov@does_not_exist.com>
2019-10-10 18:24:57 +0300

    Fix for clang-10

--- asio.cpp
+++ asio.cpp
@@ -1,193 +1,192 @@
-#include <experimental/coroutine>
-#include <iostream>
 #include <vector>
 
 #include <boost/asio.hpp>
+#include <experimental/coroutine>
 
 namespace stdx = std::experimental;
 namespace net = boost::asio;
 
-template<typename MutableBuffer>
-auto AsyncReadSome(net::ip::tcp::socket &Socket, MutableBuffer &&Buffer) {
-  struct FAwaiter {
-    FAwaiter(net::ip::tcp::socket &Socket, MutableBuffer &&Buffer) : Socket(Socket), Buffer(std::forward<MutableBuffer>(Buffer)) { }
+template <typename MutableBuffer>
+auto AsyncReadSome(net::ip::tcp::socket& Socket, MutableBuffer&& Buffer) {
+    struct FAwaiter {
+        FAwaiter(net::ip::tcp::socket& Socket, MutableBuffer&& Buffer) :
+            Socket(Socket), Buffer(std::forward<MutableBuffer>(Buffer)) {}
 
-    constexpr bool await_ready() const noexcept {
-      return false;
-    }
+        constexpr bool await_ready() const noexcept {
+            return false;
+        }
 
-    bool await_suspend(stdx::coroutine_handle<> Handle) {
-      Socket.async_read_some(Buffer, [this, Handle](auto &&Code, auto &&Received) {
-        this->Received = std::forward<decltype(Received)>(Received);
-        this->Code = std::forward<decltype(Code)>(Code);
-        Handle.resume();
-      });
-      return true;
-    }
+        bool await_suspend(stdx::coroutine_handle<> Handle) {
+            Socket.async_read_some(Buffer, [this, Handle](auto&& Code, auto&& Received) mutable {
+                this->Received = std::forward<decltype(Received)>(Received);
+                this->Code = std::forward<decltype(Code)>(Code);
+                Handle.resume();
+            });
+            return true;
+        }
 
-    auto await_resume() noexcept {
-      return std::make_pair(std::move(Received), std::move(Code));
-    }
+        auto await_resume() noexcept {
+            return std::make_pair(std::move(Received), std::move(Code));
+        }
 
-    net::ip::tcp::socket &Socket;
-    MutableBuffer Buffer;
-    std::size_t Received = 0;
-    boost::system::error_code Code;
-  };
+        net::ip::tcp::socket& Socket;
+        MutableBuffer Buffer;
+        std::size_t Received = 0;
+        boost::system::error_code Code;
+    };
 
-  return FAwaiter(Socket, std::forward<MutableBuffer>(Buffer));
+    return FAwaiter(Socket, std::forward<MutableBuffer>(Buffer));
 }
 
-template<typename ConstBuffer>
-auto AsyncWrite(net::ip::tcp::socket &Socket, ConstBuffer &&Buffer) {
-  struct FAwaiter {
-    FAwaiter(net::ip::tcp::socket &Socket, ConstBuffer &&Buffer) : Socket(Socket), Buffer(std::forward<ConstBuffer>(Buffer)) { }
+template <typename ConstBuffer>
+auto AsyncWrite(net::ip::tcp::socket& Socket, ConstBuffer&& Buffer) {
+    struct FAwaiter {
+        FAwaiter(net::ip::tcp::socket& Socket, ConstBuffer&& Buffer) :
+            Socket(Socket), Buffer(std::forward<ConstBuffer>(Buffer)) {}
 
-    constexpr bool await_ready() const noexcept {
-      return false;
-    }
+        constexpr bool await_ready() const noexcept {
+            return false;
+        }
 
-    bool await_suspend(stdx::coroutine_handle<> Handle) {
-      net::async_write(Socket, Buffer, [this, Handle](auto &&Code, auto &&Transferred) {
-        this->Transferred = std::forward<decltype(Transferred)>(Transferred);
-        this->Code = std::forward<decltype(Code)>(Code);
-        Handle.resume();
-      });
-      return true;
-    }
+        bool await_suspend(stdx::coroutine_handle<> Handle) {
+            net::async_write(Socket, Buffer, [this, Handle](auto&& Code, auto&& Transferred) mutable {
+                this->Transferred = std::forward<decltype(Transferred)>(Transferred);
+                this->Code = std::forward<decltype(Code)>(Code);
+                Handle.resume();
+            });
+            return true;
+        }
 
-    auto await_resume() noexcept {
-      return std::make_pair(std::move(Transferred), std::move(Code));
-    }
+        auto await_resume() noexcept {
+            return std::make_pair(std::move(Transferred), std::move(Code));
+        }
 
-    net::ip::tcp::socket &Socket;
-    ConstBuffer Buffer;
-    std::size_t Transferred = 0;
-    boost::system::error_code Code;
-  };
+        net::ip::tcp::socket& Socket;
+        ConstBuffer Buffer;
+        std::size_t Transferred = 0;
+        boost::system::error_code Code;
+    };
 
-  return FAwaiter(Socket, std::forward<ConstBuffer>(Buffer));
+    return FAwaiter(Socket, std::forward<ConstBuffer>(Buffer));
 }
 
-auto AsyncAccept(net::ip::tcp::acceptor &Acceptor) {
-  struct FAwaiter {
-    explicit FAwaiter(net::ip::tcp::acceptor &Acceptor) : Acceptor(Acceptor), Socket(Acceptor.get_executor().context()) { }
+auto AsyncAccept(net::ip::tcp::acceptor& Acceptor) {
+    struct FAwaiter {
+        explicit FAwaiter(net::ip::tcp::acceptor& Acceptor) : Acceptor(Acceptor), Socket(Acceptor.get_executor()) {}
 
-    constexpr bool await_ready() const noexcept {
-      return false;
-    }
+        constexpr bool await_ready() const noexcept {
+            return false;
+        }
 
-    bool await_suspend(stdx::coroutine_handle<> Handle) {
-      Acceptor.async_accept(Socket, [this, Handle](auto &&Code) {
-        this->Code = std::forward<decltype(Code)>(Code);
-        Handle.resume();
-      });
-      return true;
-    }
+        bool await_suspend(stdx::coroutine_handle<> Handle) {
+            Acceptor.async_accept(Socket, [this, Handle](auto&& Code) mutable {
+                this->Code = std::forward<decltype(Code)>(Code);
+                Handle.resume();
+            });
+            return true;
+        }
 
-    auto await_resume() noexcept {
-      return std::make_pair(std::move(Socket), std::move(Code));
-    }
+        auto await_resume() noexcept {
+            return std::make_pair(std::move(Socket), std::move(Code));
+        }
 
-    net::ip::tcp::acceptor &Acceptor;
-    net::ip::tcp::socket Socket;
-    boost::system::error_code Code;
-  };
+        net::ip::tcp::acceptor& Acceptor;
+        net::ip::tcp::socket Socket;
+        boost::system::error_code Code;
+    };
 
-  return FAwaiter(Acceptor);
+    return FAwaiter(Acceptor);
 }
 
 class FAsyncTask;
 
 struct FAsyncTaskPromise {
-  FAsyncTask get_return_object() noexcept;
+    FAsyncTask get_return_object() noexcept;
 
-  constexpr stdx::suspend_always initial_suspend() const noexcept {
-    return {};
-  }
+    constexpr stdx::suspend_always initial_suspend() const noexcept {
+        return {};
+    }
 
-  constexpr stdx::suspend_never final_suspend() const noexcept {
-    return {};
-  }
+    constexpr stdx::suspend_never final_suspend() const noexcept {
+        return {};
+    }
 
-  void unhandled_exception() noexcept { }
+    void unhandled_exception() noexcept {}
 
-  void return_void() const noexcept { }
+    void return_void() const noexcept {}
 };
 
 class FAsyncTask {
-  using PromiseType = FAsyncTaskPromise;
-  using HandleType  = stdx::coroutine_handle<PromiseType>;
+    using PromiseType = FAsyncTaskPromise;
+    using HandleType = stdx::coroutine_handle<PromiseType>;
+
 public:
-  using promise_type = PromiseType;
+    using promise_type = PromiseType;
 
-  explicit FAsyncTask(PromiseType &Promise) noexcept : Handle(HandleType::from_promise(Promise)) { }
+    explicit FAsyncTask(PromiseType& Promise) noexcept : Handle(HandleType::from_promise(Promise)) {}
 
-  FAsyncTask(FAsyncTask &&Other) noexcept : Handle(std::exchange(Other.Handle, nullptr)) { }
+    FAsyncTask(FAsyncTask&& Other) noexcept : Handle(std::exchange(Other.Handle, nullptr)) {}
 
-  ~FAsyncTask() {
-    if (Handle) {
-      Handle.destroy();
+    ~FAsyncTask() {
+        if (Handle) {
+            Handle.destroy();
+        }
     }
-  }
 
-  FAsyncTask &operator=(FAsyncTask &&Other) noexcept {
-    std::swap(Handle, Other.Handle);
-    return *this;
-  }
+    FAsyncTask& operator=(FAsyncTask&& Other) noexcept {
+        std::swap(Handle, Other.Handle);
+        return *this;
+    }
 
-  void operator()(net::io_context &Context) && {
-    net::post(Context, [Handle = std::exchange(Handle, nullptr)]() {
-      Handle.resume();
-    });
-  }
+    void operator()(net::io_context& Context) && {
+        net::post(Context, [Handle = std::exchange(Handle, nullptr)]() mutable { Handle.resume(); });
+    }
+
 private:
-  friend FAsyncTaskPromise;
+    friend FAsyncTaskPromise;
 
-  HandleType Handle;
+    HandleType Handle;
 };
 
 FAsyncTask FAsyncTaskPromise::get_return_object() noexcept {
-  return FAsyncTask(*this);
+    return FAsyncTask(*this);
 }
 
-FAsyncTask StartSession(net::io_context &Context, net::ip::tcp::socket Socket) {
-  std::vector<std::byte> Buffer(1024);
+FAsyncTask StartSession(net::io_context& Context, net::ip::tcp::socket Socket) {
+    std::vector<std::byte> Buffer(1024);
 
-  while (true) {
-    const auto [Received, Code] = co_await AsyncReadSome(Socket, net::buffer(data(Buffer), size(Buffer)));
-    if (Code) {
-      break;
+    while (true) {
+        const auto [Received, Code] = co_await AsyncReadSome(Socket, net::buffer(data(Buffer), size(Buffer)));
+        if (Code) {
+            break;
+        }
+        if (const auto [Transferred, Code] = co_await AsyncWrite(Socket, net::buffer(data(Buffer), Received)); Code) {
+            break;
+        }
     }
-    if (const auto [Transferred, Code] = co_await AsyncWrite(Socket, net::buffer(data(Buffer), Received)); Code) {
-      break;
-    }
-  }
 }
 
-FAsyncTask StartListening(net::io_context &Context, const net::ip::tcp::endpoint &Endpoint) {
-  net::ip::tcp::acceptor Acceptor(Context, Endpoint);
-  Acceptor.listen();
-  while (true) {
-    if (auto [Socket, Code] = co_await AsyncAccept(Acceptor); !Code) {
-      StartSession(Context, std::move(Socket))(Context);;
+FAsyncTask StartListening(net::io_context& Context, const net::ip::tcp::endpoint& Endpoint) {
+    net::ip::tcp::acceptor Acceptor(Context, Endpoint);
+    Acceptor.listen();
+    while (true) {
+        if (auto [Socket, Code] = co_await AsyncAccept(Acceptor); !Code) {
+            StartSession(Context, std::move(Socket))(Context);
+        }
     }
-  }
 }
 
 int main() {
-  net::io_context Context(1);
+    net::io_context Context(1);
 
-  net::signal_set Signals(Context, SIGINT, SIGTERM);
-  Signals.async_wait([&Context](auto &&, auto &&) {
-    Context.stop();
-  });
+    net::signal_set Signals(Context, SIGINT, SIGTERM);
+    Signals.async_wait([&Context](auto&&, auto&&) { Context.stop(); });
 
-  const net::ip::tcp::endpoint Endpoint(net::ip::make_address("127.0.0.1"), 3386);
+    const net::ip::tcp::endpoint Endpoint(net::ip::make_address("127.0.0.1"), 3386);
 
-  StartListening(Context, Endpoint)(Context);
+    StartListening(Context, Endpoint)(Context);
 
-  Context.run();
- 
-  return 0;
+    Context.run();
+
+    return 0;
 }
--- generator.cpp
+++ generator.cpp
@@ -1,185 +1,159 @@
 #include <cinttypes>
 #include <cstdio>
-#include <experimental/coroutine>
-#include <memory_resource>
+#include <future>
 #include <type_traits>
 #include <utility>
-#include <future>
+
+#include <experimental/coroutine>
 
 namespace stdx = std::experimental;
 
-template<typename T>
+template <typename T>
 class TGenerator;
 
-template<typename T>
+template <typename T>
 class TGeneratorPromise {
 public:
-  using ValueType     = T;
-  using ReferenceType = const ValueType &;
-  using PointerType   = const ValueType *;
+    using ValueType = T;
+    using ReferenceType = const ValueType&;
+    using PointerType = const ValueType*;
 
-  template<typename ...Args>
-  void *operator new(std::size_t Size, std::pmr::memory_resource *Resource, Args &&...) {
-    const auto Buffer = Resource->allocate(Size + sizeof(Resource), alignof(decltype(Resource)));
-    std::memcpy(Buffer, &Resource, sizeof(Resource));
-    return ((std::byte *) Buffer) + sizeof(Resource);
-  }
+    TGenerator<T> get_return_object() noexcept;
 
-  void operator delete(void *Buffer, std::size_t Size) {
-    std::pmr::memory_resource *Resource;
-    void *Source = ((std::byte *) Buffer) - sizeof(Resource);
-    std::memcpy(&Resource, Source, sizeof(Resource));
-    Resource->deallocate(Source, Size + sizeof(Resource));
-  }
+    constexpr stdx::suspend_always initial_suspend() const noexcept {
+        return {};
+    }
 
-  TGenerator<T> get_return_object() noexcept;
+    constexpr stdx::suspend_always final_suspend() const noexcept {
+        return {};
+    }
 
-  constexpr stdx::suspend_always initial_suspend() const noexcept {
-    return {};
-  }
+    template <typename E>
+    E&& await_transform(E&& Expression) = delete;
 
-  constexpr stdx::suspend_always final_suspend() const noexcept {
-    return {};
-  }
+    template <typename U>
+    stdx::suspend_always yield_value(U&& Init) noexcept(noexcept(std::declval<T&>() = std::declval<U>())) {
+        Value = std::forward<U>(Init);
+        return {};
+    }
 
-  template<typename E>
-  E &&await_transform(E &&Expression) = delete;
+    void unhandled_exception() noexcept {}
 
-  template<typename U, typename = std::enable_if_t<std::is_assignable_v<T &, U>>>
-  stdx::suspend_always yield_value(U &&Value) noexcept(noexcept(std::declval<T &>() = std::declval<U>())) {
-    this->Value = std::forward<U>(Value);
-    return {};
-  }
+    void return_void() const noexcept {}
 
-  void unhandled_exception() noexcept { }
+    decltype(auto) GetValue() const noexcept {
+        return (Value);
+    }
 
-  void return_void() const noexcept { }
-
-  decltype(auto) GetValue() const noexcept {
-    return (Value);
-  }
 private:
-  T Value;
+    T Value;
 };
 
-template<typename T>
+template <typename T>
 class TGeneratorIterator {
-  using PromiseType = TGeneratorPromise<T>;
-  using HandleType  = stdx::coroutine_handle<PromiseType>;
+    using PromiseType = TGeneratorPromise<T>;
+    using HandleType = stdx::coroutine_handle<PromiseType>;
+
 public:
-  using iterator_category = std::input_iterator_tag;
-  using difference_type   = std::ptrdiff_t;
-  using value_type        = typename PromiseType::ValueType;
-  using reference         = typename PromiseType::ReferenceType;
-  using pointer           = typename PromiseType::PointerType;
+    using iterator_category = std::input_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = typename PromiseType::ValueType;
+    using reference = typename PromiseType::ReferenceType;
+    using pointer = typename PromiseType::PointerType;
 
-  struct FSentinel { };
+    constexpr bool operator==(const TGeneratorIterator& Other) const noexcept {
+        return Handle == Other.Handle;
+    }
 
-  constexpr TGeneratorIterator() noexcept = default;
+    constexpr bool operator!=(const TGeneratorIterator& Other) const noexcept {
+        return !(*this == Other);
+    }
 
-  constexpr explicit TGeneratorIterator(HandleType Handle) noexcept : Handle(Handle) { }
+    decltype(auto) operator++() {
+        if (Handle(); Handle.done()) {
+            Handle = nullptr;
+        }
 
-  constexpr bool operator==(const TGeneratorIterator &Other) const noexcept {
-    return Handle == Other.Handle;
-  }
+        return (*this);
+    }
 
-  constexpr bool operator==(FSentinel) const noexcept {
-    return !Handle || Handle.done();
-  }
+    void operator++(int) noexcept {
+        (void) operator++();
+    }
 
-  constexpr bool operator!=(const TGeneratorIterator &Other) const noexcept {
-    return !(*this == Other);
-  }
+    decltype(auto) operator*() const noexcept {
+        return Handle.promise().GetValue();
+    }
 
-  constexpr bool operator!=(FSentinel Other) const noexcept {
-    return !(*this == Other);
-  }
+    auto operator-> () const noexcept {
+        return std::addressof(operator*());
+    }
 
-  decltype(auto) operator++() noexcept {
-    Handle.resume();
-    return (*this);
-  }
-
-  void operator++(int) noexcept {
-    (void) operator++();
-  }
-
-  decltype(auto) operator*() const noexcept {
-    return Handle.promise().GetValue();
-  }
-
-  auto operator->() const noexcept {
-    return std::addressof(operator*());
-  }
-private:
-  HandleType Handle = nullptr;
+    HandleType Handle = nullptr;
 };
 
-template<typename T>
-class [[nodiscard]] TGenerator{
-  using PromiseType = TGeneratorPromise<T>;
-  using HandleType  = stdx::coroutine_handle<PromiseType>;
+template <typename T>
+class [[nodiscard]] TGenerator {
+    using PromiseType = TGeneratorPromise<T>;
+    using HandleType = stdx::coroutine_handle<PromiseType>;
+
 public:
-  using promise_type = PromiseType;
+    using promise_type = PromiseType;
 
-  explicit TGenerator(PromiseType &Promise) noexcept : Handle(HandleType::from_promise(Promise)) { }
+    explicit TGenerator(PromiseType & Promise) noexcept : Handle(HandleType::from_promise(Promise)) {}
 
-  TGenerator(TGenerator &&Other) noexcept : Handle(std::exchange(Other.Handle, nullptr)) {
+    TGenerator(TGenerator && Other) noexcept : Handle(std::exchange(Other.Handle, nullptr)) {}
 
-  }
-
-  ~TGenerator() {
-    if (Handle) {
-      Handle.destroy();
+    ~TGenerator() {
+        if (Handle) {
+            Handle.destroy();
+        }
     }
-  }
 
-  TGenerator &operator=(TGenerator &&Other) noexcept {
-    std::swap(Handle, Other.Handle);
-    return *this;
-  }
-
-  constexpr auto begin() const noexcept {
-    TGeneratorIterator<T> Begin(Handle);
-    if (Handle) {
-      ++Begin;
+    TGenerator& operator=(TGenerator&& Other) noexcept {
+        std::swap(Handle, Other.Handle);
+        return *this;
     }
-    return Begin;
-  }
 
-  constexpr auto end() const noexcept {
-    return typename TGeneratorIterator<T>::FSentinel{};
-  }
+    constexpr auto begin() const {
+        return ++TGeneratorIterator<T>{Handle};
+    }
 
-  constexpr auto cbegin() const noexcept {
-    return begin();
-  }
+    constexpr auto end() const noexcept {
+        return TGeneratorIterator<T>{};
+    }
 
-  constexpr auto cend() const noexcept {
-    return end();
- }
+    constexpr auto cbegin() const noexcept {
+        return begin();
+    }
+
+    constexpr auto cend() const noexcept {
+        return end();
+    }
+
 private:
-  friend TGeneratorPromise<T>;
+    friend TGeneratorPromise<T>;
 
-  HandleType Handle;
+    HandleType Handle;
 };
 
-template<typename T>
+template <typename T>
 TGenerator<T> TGeneratorPromise<T>::get_return_object() noexcept {
-  return TGenerator<T>(*this);
+    return TGenerator<T>(*this);
 }
 
-static TGenerator<std::uint64_t> Fibonacci(std::pmr::memory_resource *Resource, std::uint64_t K = 9) noexcept {
-  std::uint64_t X = 0, Y = 1;
-  for (int I = 0; I < K; ++I) {
-    co_yield X = std::exchange(Y, X + Y);
-  }
+static TGenerator<std::uint64_t> Fibonacci(std::uint64_t K = 9) noexcept {
+    std::uint64_t Prev = 0, Current = 1;
+    for (int I = 0; I < K; ++I) {
+        Prev = std::exchange(Current, Prev + Current);
+        co_yield Prev;
+    }
 }
 
 int main() {
-  for (auto &&Value : Fibonacci(std::pmr::new_delete_resource())) {
-    std::printf("%" PRIu64 "\n", Value);
-  }
+    for (auto Value: Fibonacci()) {
+        std::printf("%" PRIu64 "\n", Value);
+    }
 
-  return 0;
+    return 0;
 }
